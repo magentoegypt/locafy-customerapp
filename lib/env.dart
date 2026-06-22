e42eeeda4742233a28bd21ddd14e2261e546754e
Magento Egypt <magento1team@gmail.com>
2026-06-22 07:06:20 +0300

    Fix flutter version in CI/CD to 3.44.2

--- lib/env.dart
+++ lib/env.dart
@@ -8,7 +8,7 @@ Map<String, dynamic> environment = {
   "serverConfig": {
     "type": "magento",
     "url": "https://stg.locafy.market",
-    "accessToken": "jd7u3bu9g7ca1vgocv0dvpr77xof57jf",
+    "accessToken": "9jgtmp9ghrhmrgib63grpehxxjledv03",
     'blog': "https://mstore.io",
     //"https://magentoegypt.com/blog/ar/",
     //'https://magentoegypt.com/userguide/ug/%d9%85%d9%82%d8%af%d9%85%d8%a9-%d8%b9%d9%86-%d9%82%d8%a7%d8%a6%d9%85%d8%a9-%d8%a7%d9%84%d9%85%d8%a8%d9%8a%d8%b9%d8%a7%d8%aa-sales-menu-%d9%81%d9%89-%d9%85%d8%aa%d8%ac%d8%b1-%d9%85%d8%a7%d8%ac%d9%86%d8%aa%d9%88-2/',
